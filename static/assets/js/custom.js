da06bb90340b57fa50dd0eb0e538ec2ed52d11d9
Julie Paik <juliepaik@utexas.edu>
2016-04-03 13:41:34 -0500

    added bg to restaurants pg

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -44,15 +44,15 @@ SLIDER SCRIPTS
 VAGAS SLIDESHOW SCRIPTS
 ======================================*/
 // img/1.jpg
-$.vegas('slideshow', {
-backgrounds: [
-{ src: '../static/assets/img/white.jpg', fade: 1000, delay: 9000 },
-// { src: '../static/assets/img/darkgrey.jpg', fade: 1000, delay: 9000 },
-]
-})('overlay', {
-/** SLIDESHOW OVERLAY IMAGE **/
-// src: '../static/assets/js/vegas/overlays/06.png' // THERE ARE TOTAL 01 TO 15 .png IMAGES AT THE PATH GIVEN, WHICH YOU CAN USE HERE
-});
+// $.vegas('slideshow', {
+// backgrounds: [
+// { src: '../static/assets/img/white.jpg', fade: 1000, delay: 9000 },
+// // { src: '../static/assets/img/darkgrey.jpg', fade: 1000, delay: 9000 },
+// ]
+// })('overlay', {
+// /** SLIDESHOW OVERLAY IMAGE **/
+// // src: '../static/assets/js/vegas/overlays/06.png' // THERE ARE TOTAL 01 TO 15 .png IMAGES AT THE PATH GIVEN, WHICH YOU CAN USE HERE
+// });
 
 
 /*====================================
