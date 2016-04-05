3350aeb4c1515c76ff0620a1c824269fd415d7cf
Derek Truong <derek949@cs.utexas.edu>
2016-04-05 17:04:52 -0500

    added data

--- yelpAPI.py
+++ yelpAPI.py
@@ -1,8 +1,7 @@
 from yelp.client import Client
 from yelp.oauth1_authenticator import Oauth1Authenticator
-from models import Restaurant, Attraction
-from dbops import db_create, db_read, db_read_all
-import re
+# from models import Restaurant, Attraction
+# from dbops import db_create, db_read, db_read_all, db_delete
 
 url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
 auth = Oauth1Authenticator('ITlCwpUMdoFDjqHA5EX5xw', 'VSRV728X6MmtLMxnoGTczyyriBo',
@@ -12,63 +11,60 @@ client = Client(auth)
 
 f = open("Cities.txt",'r')
 f = f.read()
-f = f.split('\n')
+cities = f.split('\n')
 
-def get_restaurants():
-	params = {
-	    'term': 'restaurants',
-	    'lang': 'en',
-	    'limit': 3
-	}
+# cities = {0: "London", 1: "Austin", 2: "Barcelona", 3: "Paris", 4: "Sydeny", 5: "New York City", 6: "Maui", 7: "Rio de Janeiro", 8: "Houston", 9: "San Francisco", 10: "Dallas", 11: "Prague", 12: "Yellowstone", 13: "Los Angeles", 14: "Vancouver", 15: "Washington D.C", 16: "U.S. Virgin Islands", 17: "Zurich", 18: "Orlando", 19: "Las Vegas", 20: "Berlin", 21: "Crete", 22: "Playa del Carmen", 23: "Puerto Rico", 24: "Rome" }
 
-	id_num = 0
-	city_id = 0
-	restaurants = {}
-	
-	for cities in f:
-		city = re.sub("_", " ", cities)
-		response = client.search(city, **params)
+# def get_restaurants():
+# 	params = {
+# 	    'term': 'restaurants',
+# 	    'lang': 'en',
+# 	    'limit': 3
+# 	}
 
-		for x in response.businesses:
-			restaurant = {"id": id_num, "name": x.name, "rating": x.rating, "categ": x.categories[0][0],
-			  	 "address": x.location.display_address[0], "city_id": city_id
-			}
+# 	id_num = 0
+# 	restaurants = {}
+# 	for i in range(0, 50):
+# 		response = client.search(cities[i], **params)
+# 		print(cities[i])
 
-			restaurants[id_num] = restaurant
-			#db_create(Restaurant(**restaurant))
-			print(restaurants[id_num]['name'])	
-			id_num += 1
-		city_id +=1
+# 		for x in response.businesses:
+# 			#dictionary of name, rating, num of ratings, address, and categories
+# 			restaurant = {"id": id_num, "name": x.name, "rating": x.rating, "categ": x.categories[0][0],
+# 			  	 "address": x.location.display_address[0], "city_id": id_num
+# 			}
 
+# 			restaurants[id_num] = restaurant
+# 			#db_create(Restaurant(**restaurant))
+# 			print(restaurants[id_num]['name'])
+# 			id_num += 1
 
 def get_attractions():
 	params = {
-	    'term': 'amusement parks',
+	    'term': 'attractions',
 	    'lang': 'en',
 	    'limit': 3
 	}
 
-	id_num = 0
-	city_id = 0
-	attractions = {}
-
-	for cities in f:
-		city = re.sub("_", " ", cities)
-		response = client.search(city, **params)
+	id_num = 1
+	attractions = []
+	for w in range(1,len(cities)+1) :
+		response = client.search(cities[w-1], **params)
 
 		for x in response.businesses:
-			attraction = {"id": id_num, "name": x.name, "rating": x.rating, "categ": x.categories[0][0],
-			  	 "num_rating": x.review_count, "lat": x.location.coordinate.latitude,
-			  	 "lon": x.location.coordinate.longitude,  "city_id": city_id 
+			#dictionary of name, rating, num of ratings, address, and categories
+			attraction = {"attraction_id": id_num, "name": x.name, "rating": x.rating, "categeory": x.categories[0][0],
+			  	 "num_rating": x.review_count, "city_id": w
 			}
 
-			attractions[id_num] = attraction
+			attractions.append(attraction)
 			#db_create(Attraction(**attraction))
-			print(attractions[id_num]['name'])
+			# print(attractions[id_num])
 			id_num += 1
-		city_id += 1
-
-get_restaurants()
+	print(attractions)
 get_attractions()
+
+
+
 
 
