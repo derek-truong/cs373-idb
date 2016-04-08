54a41e7f262613c12cf4a0862764cc0d01e37c3d
Derek Truong <derek949@cs.utexas.edu>
2016-04-07 21:27:58 -0500

    cities

--- tests.py
+++ tests.py
@@ -139,6 +139,7 @@ class FunctionalTestCase(TestCase):
         self.assertEqual(attraction.name, "House of Torment")
 
     def test_db_city_join1(self):
+        reload_data(City,"Cities.json")
         restaurant_list = db_city_join(Restaurant)
         self.assertEqual(restaurant_list[0][0].name, "Restaurant De Kas")
         self.assertEqual(restaurant_list[0][1].name, "Amsterdam")
